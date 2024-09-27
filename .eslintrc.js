8e0fc2e37bc17b6a4b8cb6473f12ade405d5ffeb
pcgilmore <paulcgilmore@gmail.com>
2024-09-27 13:02:20 -0400

    updating eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   env: {
     browser: true,
   },
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     allowImportExportEverywhere: true,
-    sourceType: 'module',
+    sourceType: "module",
     requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    "import/extensions": ["error", { js: "always" }], // require js file extensions in imports
+    "linebreak-style": ["error", "unix"], // enforce unix linebreaks
+    "no-param-reassign": [2, { props: false }], // allow modifying properties of param
+    "quotes": ["error", "double"], // allow double quotes
   },
 };
